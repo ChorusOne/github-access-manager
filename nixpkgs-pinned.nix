12e53bd4f516d98a6ad5deb619fe617b935ef1ec
Ruud van Asseldonk <ruud@chorus.one>
2022-08-11 14:34:09 +0200

    Use Nix flakes, use built-in tomllib
    Flakes are said to be the future ... the convenient "nix run" command
    from Nix 2.3 has been repurposed in later versions and there is no
    direct replacement. 2.3 is quite old by now, so jump on the flakes
    bandwagon.
    
    This also changes the Python script to no longer use the external
    "tomli" package, but to use the built-in "tomllib" instead (which is the
    former "tomli" brought into the stdlib).

--- nixpkgs-pinned.nix
+++
@@ -1,4 +0,0 @@
-import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/253aecf69ed7595aaefabde779aa6449195bebb7.tar.gz";
-  sha256 = "14szn1k345jfm47k6vcgbxprmw1v16n7mvyhcdl7jbjmcggjh4z7";
-})
