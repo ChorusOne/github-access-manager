a3680572210dc44e0f3a2c8d018e2636b209e239
Ruud van Asseldonk <ruud@chorus.one>
2022-10-11 13:42:33 +0200

    Add license

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+# Copyright 2022 Chorus One
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# A copy of the License has been included in the root of the repository.
+
 """
 Github Access Manager
 
